22e833ec64f17ef0b202f9e16429b2d4104d858e
jhansi358 <jhansirania358@gmail.com>
2025-12-15 18:17:07 +0530

    variableRUNscript

---
+++ 5varibleruntime.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo"please enter ur name:"
+read USER
+echo"please enter ur password:"
+read -s PASS
+echo"UR CREDENTIALS ARE: $USER, $PASS" #DO NOT PRINT LIKE THIS JUST FOR VALIDATION
