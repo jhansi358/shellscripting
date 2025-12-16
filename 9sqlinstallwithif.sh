c953e712121c61447336010566ea981ea2916787
jhansi358 <jhansirania358@gmail.com>
2025-12-16 16:49:03 +0530

    necommit

--- 9sqlinstallwithif.sh
+++ 9sqlinstallwithif.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 ID=$(id -u) #checking for root user
 if [ $ID -ne 0 ]
-then
-   echo "please login as root user"
-     exit 1 #otherthan zero any exit status ,script stops here.to stop it here we give any number
-   else
+  then
+    echo "please login as root user"
+    exit 1 #otherthan zero any exit status ,script stops here.to stop it here we give any number
+else
     echo " yupp,root user"
 fi
 yum install sql -y
 if [ $? -ne 0 ]
   then 
-   echo "installation of sql is failed"
+    echo "installation of sql is failed"
     exit 1
-    else
+else
     echo "installation of sql is success"
 fi    
 # if exit status = 0  previous action is success script goes to nxt step
